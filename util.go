c2d570bc6bdde036c7896e72ac2decd6d68baa6e
Brian Morton <bmorton@yammer-inc.com>
2015-09-03 00:20:45 -0700

    Add command for generating additional template types.  Rearrange and deduplicate code where possible.

---
+++ util.go
@@ -0,0 +1,158 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/url"
+	"os"
+	"os/user"
+	"path"
+	"regexp"
+	"strings"
+	"text/template"
+
+	"gopkg.in/libgit2/git2go.v22"
+)
+
+func getTemplateRepo(templateURL string) (*git.Repository, string, error) {
+	var repo *git.Repository
+	var err error
+
+	currentUser, err := user.Current()
+	if err != nil {
+		return &git.Repository{}, "", err
+	}
+
+	parsedTemplate, err := url.Parse(templateURL)
+	if err != nil {
+		return &git.Repository{}, "", err
+	}
+	path := fmt.Sprintf("%s/.construct/src/%s%s", currentUser.HomeDir, parsedTemplate.Host, parsedTemplate.Path)
+
+	fmt.Printf("Attempting to clone %s to %s...\n", templateURL, path)
+	if _, err = os.Stat(path); err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("-- Cloning %s...\n", templateURL)
+			repo, err = git.Clone(templateURL, path, &git.CloneOptions{})
+		} else {
+			return &git.Repository{}, "", err
+		}
+	} else {
+		fmt.Printf("-- Found git repository at %s...\n", path)
+		repo, err = git.OpenRepository(path)
+		if err != nil {
+			return repo, path, err
+		}
+		remote, err := repo.LookupRemote("origin")
+		if err != nil {
+			return repo, path, err
+		}
+
+		refSpecs, err := remote.FetchRefspecs()
+		if err != nil {
+			return repo, path, err
+		}
+
+		err = remote.Fetch(refSpecs, nil, "")
+		if err != nil {
+			return repo, path, err
+		}
+
+		branch, err := repo.LookupBranch("origin/master", git.BranchRemote)
+		if err != nil {
+			return repo, path, err
+		}
+		commit, err := repo.LookupCommit(branch.Target())
+		if err != nil {
+			return repo, path, err
+		}
+		tree, err := commit.Tree()
+		if err != nil {
+			return repo, path, err
+		}
+		err = repo.CheckoutTree(tree, &git.CheckoutOpts{Strategy: git.CheckoutForce})
+		if err != nil {
+			return repo, path, err
+		}
+		err = repo.SetHeadDetached(branch.Target(), nil, "")
+	}
+
+	return repo, path, err
+}
+
+func directoryWalker(templateRoot string, appPath string, view interface{}, funcMap template.FuncMap, renameMap map[string]string) func(p string, info os.FileInfo, err error) error {
+	return func(p string, info os.FileInfo, err error) error {
+		if p == templateRoot {
+			return nil
+		}
+		relativePath := subtractRoot(templateRoot, p)
+
+		if relativePath == "/instructions.tmpl" {
+			return nil
+		}
+
+		dest := appPath + relativePath
+		if strings.HasSuffix(p, ".tmpl") {
+			dest = dest[:len(dest)-len(".tmpl")]
+			for matcher, replacement := range renameMap {
+				r := regexp.MustCompile(matcher)
+				dest = r.ReplaceAllString(dest, replacement)
+			}
+			t := template.New(path.Base(p))
+			if funcMap != nil {
+				t = t.Funcs(funcMap)
+			}
+			t, err = t.ParseFiles(p)
+			if err != nil {
+				errorAndBail(err)
+			}
+			f, err := os.Create(dest)
+			if err != nil {
+				errorAndBail(err)
+			}
+			err = t.Execute(f, view)
+			if err != nil {
+				errorAndBail(err)
+			}
+			f.Close()
+		} else {
+			if info.IsDir() {
+				os.Mkdir(dest, 0755)
+			} else {
+				copyFile(p, dest)
+			}
+		}
+		fmt.Println("-- " + dest)
+		return nil
+	}
+}
+
+func copyFile(src, dst string) error {
+	s, err := os.Open(src)
+	if err != nil {
+		return err
+	}
+
+	defer s.Close()
+
+	d, err := os.Create(dst)
+	if err != nil {
+		return err
+	}
+
+	if _, err := io.Copy(d, s); err != nil {
+		d.Close()
+		return err
+	}
+
+	return d.Close()
+}
+
+func subtractRoot(root string, p string) string {
+	return strings.Replace(p, root, "", -1)
+}
+
+func errorAndBail(err error) {
+	fmt.Fprintf(os.Stderr, "Error: %s\n", err)
+	os.Exit(1)
+}
